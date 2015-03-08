82bcdefb51c8c2a42ccf183574e07261dfe5bef9
manalikapadia <manali.kaps@gmail.com>
2015-03-08 18:31:18 +0000

    update rule

--- DBTransformation/src/bham/trasformation/rules/Table2ColumnFamily.java
+++ DBTransformation/src/bham/trasformation/rules/Table2ColumnFamily.java
@@ -4,9 +4,14 @@
 package bham.trasformation.rules;
 
 import metamodel.Table;
+import nosql.Column;
 import nosql.ColumnFamily;
 import nosql.impl.ColumnFamilyImpl;
+
+import org.eclipse.emf.common.util.EList;
+
 import uk.ac.bham.sitra.Rule;
+import uk.ac.bham.sitra.RuleNotFoundException;
 import uk.ac.bham.sitra.Transformer;
 
 /**
@@ -28,10 +33,22 @@ public class Table2ColumnFamily implements Rule<Table, ColumnFamily> {
 
 	public void setProperties(ColumnFamily target, Table source, Transformer t) {
 		// TODO Auto-generated method stub
-		target.setName(source.getName());
-		//target.setPK();
-		//get options from UI
-		//target.setOptions(null);
+		try {
+			target.setName(source.getName());
+			target.setColumns((EList<Column>)t.transformAll(SqlCol2NoSqlCol.class, source.getColumns()));
+			
+			//to be impl by krishna
+			//target.setPK(null);
+			
+			//for transforming rows
+			//target.setRows(null);
+			
+			//get options from UI
+			//target.setOptions(null);
+		} catch (RuleNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
