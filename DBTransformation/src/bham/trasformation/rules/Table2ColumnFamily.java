4144ec70e57a693e7f1904bbd6b75fe18493f77d
Xayna <zainamalhis@hotmail.com>
2015-03-08 23:25:45 +0000

    modify SqlCol2NoSqlCol , move the properties to setproperties modify Table2ColumnFamily, implementing the foreign key

--- DBTransformation/src/bham/trasformation/rules/Table2ColumnFamily.java
+++ DBTransformation/src/bham/trasformation/rules/Table2ColumnFamily.java
@@ -3,10 +3,14 @@
  */
 package bham.trasformation.rules;
 
+import metamodel.Constraint;
+import metamodel.ConstraintType;
 import metamodel.Table;
 import nosql.Column;
 import nosql.ColumnFamily;
+import nosql.PK;
 import nosql.impl.ColumnFamilyImpl;
+import nosql.impl.PKImpl;
 
 import org.eclipse.emf.common.util.EList;
 
@@ -16,7 +20,7 @@ import uk.ac.bham.sitra.Transformer;
 
 /**
  * @author Manali
- *
+ * 
  */
 public class Table2ColumnFamily implements Rule<Table, ColumnFamily> {
 
@@ -35,21 +39,69 @@ public class Table2ColumnFamily implements Rule<Table, ColumnFamily> {
 		// TODO Auto-generated method stub
 		try {
 			target.setName(source.getName());
-			target.setColumns((EList<Column>)t.transformAll(SqlCol2NoSqlCol.class, source.getColumns()));
-			
-			//to be impl by krishna
-			//target.setPK(null);
-			
-			//for transforming rows
-			//target.setRows(null);
-			
-			//get options from UI
-			//target.setOptions(null);
+			target.setColumns((EList<Column>) t.transformAll(
+					SqlCol2NoSqlCol.class, source.getColumns()));
+
+			// to be impl by krishna
+			// target.setPK(null);
+
+			// for transforming rows
+			// target.setRows(null);
+
+			// get options from UI
+			// target.setOptions(null);
+
+			for (Constraint constraint : source.getConstraints()) {
+				switch (constraint.getType().getValue()) {
+				case ConstraintType.COMPOSITE_PRIMARY_KEY_VALUE:
+					// to be implemented
+					break;
+
+				case ConstraintType.PRIMARY_KEY_VALUE:
+					// to be implemented
+					break;
+				case ConstraintType.UNIQUE_VALUE:
+					// to be implemented
+					break;
+
+				case ConstraintType.FOREIGN_KEY_VALUE:
+					// create new column family
+					ColumnFamily ref = new ColumnFamilyImpl();
+
+					// initialize the PK and set the reference table name
+					PK primeyKey = new PKImpl();
+					
+					// the assumption that each foreign key 
+					// should reference to one pk key in sql
+					// but in case we have more than one pk in the reference
+					for (metamodel.Column col : constraint.getReferences()) {
+						// the column name to primary key list
+						primeyKey.getReference().add(col.getName());
+						
+						// create primary key col.
+						nosql.Column pkCol = new nosql.impl.ColumnImpl();
+						pkCol.setDatatype(DatatypeMapping.getType(col.getType()));
+						pkCol.setName(col.getName());
+						pkCol.setPK(true);
+						pkCol.setSize(col.getSize());
+						pkCol.setSth(ref);
+						
+						// add column to the family column.
+						ref.getColumns().add(pkCol);
+					}
+
+					ref.setPK(primeyKey);
+					break;
+				default:
+					break;
+				}
+
+			}
 		} catch (RuleNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
