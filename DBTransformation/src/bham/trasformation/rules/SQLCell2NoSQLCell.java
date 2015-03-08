b8d135e9c17fc0933877aedb0e8206da37d1b9d8
krisgoks <kris.gokhale@gmail.com>
2015-03-08 15:04:38 +0000

    The following rule has been added: SQLCell2NoSQLCell -> which maps the cells of the SQL to NoSQL

---
+++ DBTransformation/src/bham/trasformation/rules/SQLCell2NoSQLCell.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package bham.trasformation.rules;
+
+import nosql.impl.CellImpl;
+import metamodel.Cell;
+import uk.ac.bham.sitra.Rule;
+import uk.ac.bham.sitra.Transformer;
+
+/**
+ * @author kris
+ *
+ */
+public class SQLCell2NoSQLCell implements Rule<metamodel.Cell , nosql.Cell> {
+
+	/* (non-Javadoc)
+	 * @see uk.ac.bham.sitra.Rule#check(java.lang.Object)
+	 */
+	public boolean check(Cell source) {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see uk.ac.bham.sitra.Rule#build(java.lang.Object, uk.ac.bham.sitra.Transformer)
+	 */
+	public nosql.Cell build(Cell source, Transformer t) {
+		nosql.Cell cell = new CellImpl();
+		return cell;
+	}
+
+	/* (non-Javadoc)
+	 * @see uk.ac.bham.sitra.Rule#setProperties(java.lang.Object, java.lang.Object, uk.ac.bham.sitra.Transformer)
+	 */
+	public void setProperties(nosql.Cell target, Cell source, Transformer t) {
+		
+		if (source.getColumn().getName().equals(target.getColumn().getName()))
+		target.setValue(source.getValue());		
+	}
+	
+
+}
